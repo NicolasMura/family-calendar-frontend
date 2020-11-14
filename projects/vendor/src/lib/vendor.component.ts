52d4e28c118f5ae0919e5dc4e0304b9da2f1aa53
NicolasMura <contact@nicolasmura.fr>
2020-11-14 14:36:14 +0100

    First commit

---
+++ projects/vendor/src/lib/vendor.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-vendor',
+  template: `
+    <p>
+      vendor works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class VendorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/vendor/src/lib/vendor.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { VendorComponent } from './vendor.component';
+
+
+
+@NgModule({
+  declarations: [VendorComponent],
+  imports: [
+  ],
+  exports: [VendorComponent]
+})
+export class VendorModule { }
